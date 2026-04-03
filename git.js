56bf1ea78ee70e842bb9e1b98c4cc5632b11609b
Anshul Soni <anshul.soni.dev@gmail.com>
2026-04-03 17:17:52 +0530

    all variables

--- git.js
+++ git.js
@@ -3,3 +3,16 @@ function sum(a,b){
     console.log(c);
     
 }
+
+
+let name = "Anshul";   // string
+let age = 22;          // number
+let isPlaced = false;  // boolean
+let project = null;    // no project yet
+let salary;            // undefined
+
+console.log(name);
+console.log(age);
+console.log(isPlaced);
+console.log(project);
+console.log(salary);
